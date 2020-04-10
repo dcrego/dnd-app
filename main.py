273b5facdeb112b6a74da0b2e8e3b4f585a618c2
Daniel Crego <32107291+dcrego@users.noreply.github.com>
2020-04-10 09:35:38 +0200

    Show character box

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+def show_character_box(character:dict) -> None:
+  global win, current_box
+  win.remove(current_box)
+  from boxes import CharacterBox
+  current_box = CharacterBox(character)
+  win.add(current_box)
+  win.show_all()
+
 def on_activate(app:Gtk.Application) -> None:
   winsize = {
     'default-width': 400,
     'default-height': 600
   }
+  global win, current_box
   win = Gtk.ApplicationWindow(application=app, title='D&D Companion', **winsize)
   from boxes import CharactersBox
-  win.add(CharactersBox())
+  current_box = CharactersBox(show_character_box)
+  win.add(current_box)
   win.show_all()
 
 def main() -> None:
